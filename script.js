df4d58a6f658420293ac8d04b17a05ad0fb53333
Pavithr Kumar <136888449+Pavithr-Kumar@users.noreply.github.com>
2023-09-05 18:19:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function show(){
+   //document.getElementById("navbar").style.display="block";
+   document.getElementById("navbar").style.right="0px";
+   document.getElementById("navbar").style.display="block";
+    document.getElementById("x-icon").style.display="block";
+
+  //  document.getElementById("navbar").style.right="0px";
+
+}
+function hide(){
+    document.getElementById("navbar").style.right="-200px";
+    document.getElementById("navbar").style.display="none";
+    
+}
